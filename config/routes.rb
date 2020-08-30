20bc21566bce3dabbdaa448cbfd1c1d59d5e01a4
Daniel Morales <moralesaguilerad@gmail.com>
2020-08-30 13:11:51 -0400

    Initial commit and devise implementation

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get 'home/index'
+  root to: 'home#index' #crea un root para la inicializacion del sitio.
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
