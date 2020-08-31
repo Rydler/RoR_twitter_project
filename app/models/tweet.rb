8a3dfff31b0d8f06b1f8163146af1d759bcc935a
Daniel Morales <moralesaguilerad@gmail.com>
2020-08-30 22:01:54 -0400

    update module 1

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -5,7 +5,52 @@ class Tweet < ApplicationRecord
   has_many :likes
   has_many :liking_users, :through => :likes, :source => :user
 
-  paginates_per 2
+  paginates_per 4
 
- 
+  def is_liked?(user)
+    if self.liking_users.include?(user)
+      true
+    else
+      false
+    end
+  end
+
+
+  def remove_like(user)
+    Like.where(user: user, tweet: self).first.destroy
+
+  end
+
+  def add_like(user)
+    Like.create(user: user, tweet: self)
+  end
+
+  def tweet_icon(user)
+    if is_liked?(user)
+      'fas fa-heart' #rojo
+    else
+      'far fa-heart'
+    end
+  end
+
+  def count_rt
+    Tweet.where(rt_ref: self.id).count
+  end
+
+  def is_retweet?
+    rt_ref ? true : false
+  end
+
+  def tweet_ref
+    Tweet.find(self.rt_ref)
+  end
+
+  def retweet_icon
+    if is_retweet?
+      'text-primary'
+    else
+      'text-muted'
+    end
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_30_172817) do
+ActiveRecord::Schema.define(version: 2020_08_30_230729) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2020_08_30_172817) do
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "rt_ref"
     t.index ["user_id"], name: "index_tweets_on_user_id"
   end
 
